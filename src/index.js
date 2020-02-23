2ca8f4360182632dc79f33de2c5896a39e1c3ccb
Aidan Tatysheva <tatyshevaaidan@gmail.com>
2020-02-23 16:35:48 +0600

    solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined){
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  if (array === undefined){
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
+  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+  if (array === undefined){
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let item of array) {
+    sum = sum + item;
+  }
+  return sum / array.length
 }
